992b6fd55e9db68c25666eec2f9e613b896f33be
0xantman <antho74830@gmail.com>
2019-11-19 13:55:12 +0100

    Fixes require api

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const inboxesRouter = require('./inboxes');
 const userRouter = require('./user');
-const installRouter = require('./install');
+//const installRouter = require('./install');
 
 router.use('/api', inboxesRouter);
 router.use('/admin', userRouter);
